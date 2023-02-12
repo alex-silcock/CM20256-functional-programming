04f94f1aebf20a153aa8f567cb0accf841756c27
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-02-12 14:00:02 +0000

    Swapping elements in a list of pairs and currying

--- playground.hs
+++ playground.hs
@@ -24,3 +24,27 @@ grab n (x:xs)
 
 -- fst (x, y) returns the first element, x
 -- snd (x, y) returns the second element, y
+
+swap :: (Int, String) -> (String, Int)
+swap (x, y) = (y, x)
+
+-- Invert two elements in a list of pairs
+listOfPairs :: [(Int, String)]
+listOfPairs =
+    [(100, "Hello")
+    ,(200, "World")
+    ,(300, "Haskell") 
+    ]
+
+-- Using !! for indexing, e.g. listOfPairs!!2 returns (300, "Haskell")
+
+-- Invert each element in a list of pairs
+invert :: [(Int, String)] -> [(String, Int)]
+invert [] = []
+invert ((x,y):xs) = (y,x) : invert xs
+
+exampleUncurry :: (Int -> Int -> Int) -> (Int, Int) -> Int
+exampleUncurry f (x,y) = f x y
+
+exampleCurry :: ((Int,Int) -> Int) -> Int -> Int -> Int
+exampleCurry f x y = f (x,y)
