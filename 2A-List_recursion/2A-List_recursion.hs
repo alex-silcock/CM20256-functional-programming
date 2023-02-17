6ff02d2266c9a16b69c885b1fbff86a0e99bb00e
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-02-17 13:04:54 +0000

    Exercise 2 Tutorial 2 completed

--- 2A-List_recursion/2A-List_recursion.hs
+++ 2A-List_recursion/2A-List_recursion.hs
@@ -25,14 +25,20 @@ remove (x:xs) y
 ------------------------- Exercise 2
 
 members :: [String] -> [String] -> Bool
-members xs    []  = undefined
-members xs (y:ys) = undefined
+members xs    []  = True
+members xs (y:ys)
+    | member xs y = members xs ys
+    | otherwise = False
+
 
 members' :: [String] -> [String] -> Bool
-members' = undefined
+members' xs [] = True
+members' xs (y:ys) = member xs y && members xs ys
+
 
 removeAll :: [String] -> [String] -> [String]
-removeAll = undefined
+removeAll xs [] = xs
+removeAll (x:xs) (y:ys) = removeAll (remove (x:xs) y) ys
 
 
 ------------------------- Exercise 3
