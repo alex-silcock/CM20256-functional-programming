d6561816b222e8409c95aa7fe836061b995428a6
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-02-20 15:51:03 +0000

    error handling for ex 1 and part of ex 2

--- 2B-Type_classes/2B-Type_classes.hs
+++ 2B-Type_classes/2B-Type_classes.hs
@@ -1,12 +1,9 @@
-import Distribution.Simple.Utils (xargs)
-
 
 ladies    = ["Mary","Kitty","Lydia","Elizabeth","Jane"]
 gentlemen = ["Charles","Fitzwilliam","George","William"]
 couples   = [("Elizabeth","Fitzwilliam"),("Charlotte","William"),("Lydia","George"),("Jane","Charles")]
 
 ------------------------- Exercise 1
-
 
 ditch :: Int -> [a] -> [a]
 ditch _ [] = []
@@ -16,17 +13,21 @@ ditch n (x:xs)
 
 at :: [a] -> Int -> a
 at (x:xs) n
-  | n == 0 = x
+  | n < 0     = error "at: n must be positive"
+  | n == 0    = x
   | otherwise = at xs (n-1)
-
 
 ------------------------- Exercise 2
 
 find :: Eq a => a -> [(a,b)] -> b
-find = undefined
+find s []     = error "at: element not found"
+find s ((x,y):xs)
+  | s == x    = y
+  | otherwise = find s xs
 
 which :: Eq a => a -> [a] -> Int
-which = aux 0
+which _ [] = error "empty list"
+which s arr = undefined
   where
     aux :: Eq a => Int -> a -> [a] -> Int
     aux = undefined
