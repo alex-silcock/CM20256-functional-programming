a6b215057e01a72f2fdd8b24f07b9479b8d834fd
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-05-19 12:17:47 +0100

    haskell year complete

--- playground.hs
+++ playground.hs
@@ -1,4 +1,4 @@
-import Data.Char ( isUpper, toLower, toUpper, isAlpha )
+import Data.Char ( isUpper, toLower, toUpper, isAlpha, isDigit, digitToInt )
 import Data.List ( sort )
 
 countdown :: Int -> [Int]
@@ -68,8 +68,6 @@ list2 = [5, 9, 3, 46, 1, 10, 200, 30, 99]
 evens :: [Int] -> [Int]
 evens = filter even
 
-
-
 addLists :: [Int] -> [Int] -> [Int]
 addLists xs ys = map plus (zip xs ys)
     where
@@ -132,4 +130,117 @@ foldLength = foldr (\x  -> (+) 1) 0
 capitals :: String -> [Int]
 capitals xs = [fst x | x <- zip [0..(length xs)] xs, isUpper (snd x)]
 
+getNumberFromString :: String -> Int
+getNumberFromString x = read $ filter (\x -> x `elem` ['0'..'9']) x
 
+listOf :: Int -> [Int]
+listOf n = filter even [1..n]
+
+doubleComp :: [Int] -> [Int]
+doubleComp xs = [x*2 | x <- xs]
+
+getCount :: String -> Int
+getCount = foldr (\c acc -> if c `elem` "aeiou" then acc + 1 else acc) 0
+
+getEvenNums :: [Int] -> [Int]
+getEvenNums xs = [x | x <- xs, even x]
+
+getVowels :: String -> String
+getVowels str = [s | s <- str, s `elem` "aeiouAEIOU"]
+
+sumEvenNums :: [Int] -> Int
+sumEvenNums xs = sum [x | x <- xs, even x]
+
+getCommonElems :: [Int] -> [Int] -> [Int]
+getCommonElems str1 str2 = [c1 | c1 <- str1, c2 <- str2, c1 == c2]
+
+getLongStrings :: [String] -> [String]
+getLongStrings xs = [x | x <- xs, length x > 5]
+
+sumDigits :: String -> Int
+sumDigits "" = 0
+sumDigits (s:ss)
+  | isDigit s = digitToInt s + sumDigits ss
+  | otherwise = sumDigits ss
+
+sumDigits2 :: String -> Int
+sumDigits2 str = sum [digitToInt c | c <- str, isDigit c]
+
+sumDigits3 :: String -> Int
+sumDigits3 str = foldr ((+) . digitToInt) 0 (filter isDigit str)
+
+shortLengths :: [String] -> Int
+shortLengths xs = foldr (+) (0) (filter (<=5) (map length xs))
+
+shorts :: [String] -> [String]
+shorts = filter (\s -> length s <= 5)
+
+addToEnd :: Int -> [Int] -> [Int]
+addToEnd = foldr (:) . (:[])
+
+rev :: [Int] -> [Int]
+rev = foldr addToEnd []
+
+s :: [Int] -> Int
+s = foldl (-) 0
+
+s2 :: [Int] -> Int
+s2 = foldr (-) 0
+
+thatlist :: [Int]
+thatlist = map f (filter even [0..10])
+  where f n = 3*n + 1
+
+stillthatlist :: [Int]
+stillthatlist = map (\s -> 3*s + 1) (filter even [0..10])
+
+thisisstillthatlist :: [Int]
+thisisstillthatlist = [3*n + 1 | n <- [0..10], even n]
+
+reverseShorts :: [String] -> [String]
+reverseShorts [] = []
+reverseShorts (x:xs)
+  | length x <= 5 = reverseShorts xs ++ [x]
+  | otherwise     = reverseShorts xs
+
+reverseShorts2 :: [String] -> [String]
+reverseShorts2 xs = reverse [x | x <- xs, length x <= 5]
+
+reverseShorts3 :: [String] -> [String]
+reverseShorts3 = reverse . filter f
+  where f x = length x <= 5
+
+reverseShorts4 :: [String] -> [String]
+reverseShorts4 = reverse . filter (\x -> length x <= 5)
+
+sayHello :: IO String
+sayHello =
+  do
+    putStrLn "Hello"
+    getLine
+
+count :: Int -> IO ()
+count x = do
+  print x
+  if x == 0
+  then do
+    putStrLn "ZERO"
+  else do
+    count (x-1)
+
+afact :: Int -> Int -> Int
+afact a 0 = a
+afact a n = afact (a*n) (n-1)
+
+areverse :: [Int] -> [Int] -> [Int]
+areverse a []     = a
+areverse a (x:xs) = areverse (x:a) xs
+
+sL :: [String] -> Int
+sL = foldr (+) 0 . map length . filter (\x -> length x <= 5)
+
+sumDigits'' :: String -> Int
+sumDigits'' xs = sum [digitToInt x | x <- xs, isDigit x]
+
+snoc :: Int -> [Int] -> [Int]
+snoc = foldr (:) . (:[])
